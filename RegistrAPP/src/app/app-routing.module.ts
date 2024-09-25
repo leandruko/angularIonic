a7717bac54d94dd4d7ef0ab42d0117a9f9369174
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-09-25 12:54:55 -0300

    update rutas

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -1,30 +1,15 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactPageModule) }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
