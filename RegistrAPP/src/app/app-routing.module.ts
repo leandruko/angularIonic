6c9bb1cf82c5edeeb73ffe26ad705de2153d4270
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-09-26 16:53:03 -0300

    update rutas y estructura

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -2,25 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirigir al login cuando la app inicie
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
+  { path: 'docente', loadChildren: () => import('./docente/docente.module').then(m => m.DocentePageModule) },
+  { path: 'alumno', loadChildren: () => import('./alumno/alumno.module').then(m => m.AlumnoPageModule) },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
