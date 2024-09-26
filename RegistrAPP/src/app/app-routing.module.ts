2510c65983cc8291411d28df32838c6a2767cab5
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-09-26 16:44:18 -0300

    update de repositorio

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -3,26 +3,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then(m => m.ContactPageModule)
-  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- RegistrAPP/src/app/login/login.module.ts
+++ RegistrAPP/src/app/login/login.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ContactPageRoutingModule } from './contact-routing.module';
+import { LoginPageRoutingModule } from './login-routing.module';
 
-import { ContactPage } from './contact.page';
+import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ContactPageRoutingModule
+    LoginPageRoutingModule
   ],
-  declarations: [ContactPage]
+  declarations: [LoginPage]
 })
-export class ContactPageModule {}
+export class LoginPageModule {}
